f09c59e55cedaae2ea04bf30b00966de8c1de2db
yurkimus <yurkimus@gmail.com>
2025-11-16 02:35:50 +0100

    Initial

---
+++ generate.js
@@ -0,0 +1,59 @@
+import { Method, Methods, Mode, Roles, Routes } from '@shakerquiz/utilities'
+
+let identifier = (method, route, role) => method + '_' + route.replaceAll('/', '_') + '_' + role
+
+let property = (method, route, role) => method + '/' + route + '/' + role
+
+let pathnames = Methods
+  .flatMap(method => Routes.map(route => [method, route]))
+  .flatMap(([method, route]) => Roles.concat(Mode['Unknown']).map(role => [method, route, role]))
+  .map(([method, route, role]) => ({ method, route, role }))
+
+Promise
+  .all(pathnames.map(({ method, route, role }) =>
+    Deno
+      .lstat('./source/schemas/' + method + '/' + route + '/' + role + '.json')
+      .then(() => ({ method, route, role }))
+      .catch(() => null)
+  ))
+  .then(components =>
+    Deno.writeTextFile(
+      './source/index.js',
+      ''
+        + "import { Method, Methods, Mode, Roles, Routes } from '@shakerquiz/utilities'"
+        + '\n\n'
+        + components
+          .filter(Boolean)
+          .reduce((text, { method, route, role }) =>
+            text
+            + 'import'
+            + ' '
+            + identifier(method, route, role)
+            + ' '
+            + 'from'
+            + "'"
+            + './schemas/' + method + '/' + route + '/' + role + '.json'
+            + "'"
+            + ' '
+            + "with { type: 'json' }"
+            + '\n', '')
+        + '\n'
+        + 'const Schema = Object.freeze({'
+        + '\n'
+        + components
+          .filter(Boolean)
+          .reduce((text, { method, route, role }) =>
+            text
+            + '\t'
+            + "'"
+            + property(method, route, role)
+            + "'"
+            + ':'
+            + ' '
+            + identifier(method, route, role)
+            + ','
+            + '\n', '')
+        + '})'
+        + '\n',
+    )
+  )
